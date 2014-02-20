7b6e6b831d4f4223f89999fb2f659b8c002b3c96
Sebastian Klatt <sebastian@markow.org>
2014-02-20 11:36:39 +0100

    Added function for computing uv bounds of a mesh

--- src/pam/helper.py
+++ src/pam/helper.py
@@ -40,20 +40,53 @@ class PAMTestOperator(bpy.types.Operator):
     def execute(self, context):
         active_obj = context.active_object
 
-        rasterize_uv(active_obj, None)
+        x, y = uvpoint_from_origin_with_dist(active_obj, (0.3, 0.5), None)
 
-        result = {'FINISHED'}
-        logger.debug("%s: %s", self.__class__.__name__, result)
-        return result
+        return {'FINISHED'}
 
 
-# TODO(SK): rasterize_uv missing docstring
+# TODO(SK): missing docstring
 @utils.profiling
 def rasterize_uv(obj, resolution):
     pass
 
 
-# TODO(SK): rand_uvpoint_from_origin missing docstring
+# TODO(SK): missing docstring
+# TODO(SK): needs implementation
 @utils.profiling
-def rand_uvpoint_from_origin(obj, origin, func):
-    obj.data.uv_layers.active.data
+def uvpoint_from_origin_with_dist(obj, origin, dist_func):
+    x_max, y_max = uv_bounds(obj)
+    x, y = origin
+
+    x_random = 0.0
+    y_random = 0.0
+
+    logger.debug(
+        "x: %f, y: %f, rx: %f, ry: %f, dist_func: %s",
+        x,
+        y,
+        x_random,
+        y_random,
+        dist_func
+    )
+
+    return None, None
+
+
+# TODO(SK): missing docstring
+@utils.profiling
+def uv_bounds(obj):
+    active_uv = obj.data.uv_layers.active
+
+    if not hasattr(active_uv, "data"):
+        raise Exception("%s has no uv data", obj)
+
+    x_values = [mesh.uv[0] for mesh in active_uv.data]
+    y_values = [mesh.uv[1] for mesh in active_uv.data]
+
+    x_max = max(x_values)
+    y_max = max(y_values)
+
+    logger.debug("max(x): %f, max(y): %f", x_max, y_max)
+
+    return x_max, y_max

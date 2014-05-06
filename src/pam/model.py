1ade1de9fe29d12d3dc1fce0ac20ac1cc6fc475b
Martin Pyka <m.pyka@rub.de>
2014-05-06 13:53:07 +0200

    added support for saving via pickle but does not work so far

--- src/pam/model.py
+++ src/pam/model.py
@@ -1,6 +1,7 @@
 """Data model module"""
 
 import pickle
+import copy
 
 NG_LIST = []
 NG_DICT = {}
@@ -10,6 +11,29 @@ CONNECTIONS = []
 CONNECTION_RESULTS = []
 
 
+def convertObject2String(connection):
+    """ Takes a CONNECTION-struct and converts bpy.objects to 
+    string names and returns a list of stirngs """
+    return [o.name for o in connection[0]]
+
+def makeConnectionPickleReady(connections):
+    result = []
+    for c in connections:
+        new_c = (convertObject2String(c))
+        new_c = new_c + c[1:]
+        result.append(new_c)
+    return result
+
+def convertVector2Array(connection_results):
+    """ Takes a CONNECTION_RESULTS-struct and converts mathutils.Vectors
+    to numpy.arrays and returns them """
+    result = []
+    for c in connection_results:
+        result.append(numpy.array(c['s']))
+    return result
+
+
+
 # TODO(SK): missing docstring
 class ModelSnapshot(object):
     def __init__(self):
@@ -17,8 +41,8 @@ class ModelSnapshot(object):
         self.NG_DICT = NG_DICT
         self.CONNECTION_COUNTER = CONNECTION_COUNTER
         self.CONNECTION_INDICES = CONNECTION_INDICES
-        self.CONNECTIONS = CONNECTIONS
-        self.CONNECTION_RESULTS = CONNECTION_RESULTS
+        self.CONNECTIONS = copy.deepcopy(CONNECTIONS)
+        self.CONNECTION_RESULTS = copy.deepcopy(CONNECTION_RESULTS)
 
 
 # TODO(SK): missing docstring

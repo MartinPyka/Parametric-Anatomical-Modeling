c254adb15d1330bef66044d3ea6d7ca640e02411
Sebastian Klatt <sebastian@markow.org>
2014-07-01 14:18:42 +0200

    Refactored unittest

--- src/pam/unittest.py
+++ src/pam/unittest.py
@@ -1,75 +1,78 @@
+import logging
+import pickle
+import random
+
 import bpy
+import numpy
+
 from pam import pam
 from pam import pam_vis
 from pam.kernel import gaussian as kernel
 from pam import model
-import random
-import numpy
 
-import pickle
-
-EXPORT_PATH = '/home/martin/ownCloud/work/Projekte/hippocampal_model/results/'
-
-p1 = bpy.data.objects['Plane']
-p2 = bpy.data.objects['Plane.001']
-p3 = bpy.data.objects['Plane.002']
-p4 = bpy.data.objects['Plane.003']
-p5 = bpy.data.objects['Plane.004']
-p6 = bpy.data.objects['Plane.005']
+logger = logging.getLogger(__package__)
 
 
-pam_vis.visualizeClean()
-pam.initialize3D()
+if __name__ == "__main__":
+    random.seed(1)
 
-random.seed(1)
+    EXPORT_PATH = '/home/martin/ownCloud/work/Projekte/hippocampal_model/results/'
 
-id1 = pam.addConnection(
-    [p1, p2, p3],
-    'ParticleSystem', 'ParticleSystem',
-    1,
-    [pam.MAP_top, pam.MAP_top],
-    [pam.DIS_jumpUV, pam.DIS_euclid],
-    kernel.gauss,
-    [0.2, 0.2, 0., 0.],
-    kernel.gauss,
-    [0.2, 0.2, 0., 0.],
-    1
-)
+    p1 = bpy.data.objects['Plane']
+    p2 = bpy.data.objects['Plane.001']
+    p3 = bpy.data.objects['Plane.002']
+    p4 = bpy.data.objects['Plane.003']
+    p5 = bpy.data.objects['Plane.004']
+    p6 = bpy.data.objects['Plane.005']
 
-# id2 = pam.addConnection(
-#    [p4, p5, p6],
-#    'ParticleSystem', 'ParticleSystem',
-#   1,
-#   [pam.MAP_uv, pam.MAP_uv],
-#    [pam.DIS_jumpUV, pam.DIS_euclid],
-#    kernel.gauss,
-#    [0.2, 0.2, 0., 0.],
-#    kernel.gauss,
-#    [0.2, 0.2, 0., 0.],
-#    1
-#    )
+    pam_vis.visualizeClean()
+    pam.initialize3D()
 
-pam.computeAllConnections()
-path = pam_vis.visualizeConnectionsForNeuron(id1, 8)
+    id1 = pam.addConnection(
+        [p1, p2, p3],
+        'ParticleSystem', 'ParticleSystem',
+        1,
+        [pam.MAP_top, pam.MAP_top],
+        [pam.DIS_jumpUV, pam.DIS_euclid],
+        kernel.gauss,
+        [0.2, 0.2, 0., 0.],
+        kernel.gauss,
+        [0.2, 0.2, 0., 0.],
+        1
+    )
 
-create_reference = True
+    # id2 = pam.addConnection(
+    #    [p4, p5, p6],
+    #    'ParticleSystem', 'ParticleSystem',
+    #   1,
+    #   [pam.MAP_uv, pam.MAP_uv],
+    #    [pam.DIS_jumpUV, pam.DIS_euclid],
+    #    kernel.gauss,
+    #    [0.2, 0.2, 0., 0.],
+    #    kernel.gauss,
+    #    [0.2, 0.2, 0., 0.],
+    #    1
+    #    )
 
+    pam.computeAllConnections()
+    path = pam_vis.visualizeConnectionsForNeuron(id1, 8)
 
-if create_reference:
-    reference = model.save(EXPORT_PATH + 'test_universal.pam')
+    create_reference = True
 
-    f = open(EXPORT_PATH + 'test_universal.path', 'wb')
-    pickle.dump(numpy.array(path), f)
+    if create_reference:
+        reference = model.save(EXPORT_PATH + 'test_universal.pam')
+
+        f = open(EXPORT_PATH + 'test_universal.path', 'wb')
+        pickle.dump(numpy.array(path), f)
+        f.close()
+
+    reference = model.load(EXPORT_PATH + 'test_universal.pam')
+    result = model.ModelSnapshot()
+
+    logger.info('Connections: ', str(reference.CONNECTION_RESULTS) == str(result.CONNECTION_RESULTS))
+
+    f = open(EXPORT_PATH + 'test_universal.path', 'rb')
+    reference_path = pickle.load(f)
     f.close()
 
-
-reference = model.load(EXPORT_PATH + 'test_universal.pam')
-result = model.ModelSnapshot()
-
-print('Connections: ', str(reference.CONNECTION_RESULTS) == str(result.CONNECTION_RESULTS))
-
-f = open(EXPORT_PATH + 'test_universal.path', 'rb')
-reference_path = pickle.load(f)
-f.close()
-
-print('Path: ', (path == reference_path).all())
+    logger.info('Path: ', (path == reference_path).all())

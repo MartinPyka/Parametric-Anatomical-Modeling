04406454c35812e667b1564b0ceea019cc6619a2
Sebastian Klatt <sebastian@markow.org>
2014-07-01 18:24:44 +0200

    Refactored test.py

--- src/test/test.py
+++ src/test/test.py
@@ -6,8 +6,7 @@ sys.path.append(os.path.dirname(__file__))
 
 from test_model import TestModelComparison
 
-suite = unittest.TestSuite()
-
-suite.addTest(unittest.makeSuite(TestModelComparison))
-
-unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestModelComparison))
+    unittest.TextTestRunner(verbosity=1).run(suite)

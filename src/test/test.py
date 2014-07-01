f899c45ad3cafb470538cb67eae7bd5eb1b0d862
Sebastian Klatt <sebastian@markow.org>
2014-07-01 18:59:30 +0200

    Added pam src location to python path, so we don't have to have pam enabled as a plugin in blender.

--- src/test/test.py
+++ src/test/test.py
@@ -3,6 +3,7 @@ import sys
 import unittest
 
 sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir, "pam"))
 
 from test_model import TestModelComparison
 

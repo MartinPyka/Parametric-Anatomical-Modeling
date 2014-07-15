58f9aa7ba18ae260f6cec34e5719a24b45fc158a
Sebastian Klatt <sebastian@markow.org>
2014-07-15 15:27:58 +0200

    Added Network and Neuron model objects scaffolding

--- pam/model2.py
+++ pam/model2.py
@@ -1,1 +1,75 @@
-"""Data model module"""
+"""Model data module"""
+
+
+class Network(object):
+    """Network model data"""
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __eq__(self, other):
+        """Checks equality
+
+        Example:
+            m = Model()
+            b = Model()
+            a == b
+
+        Result:
+            bool
+        """
+        return (isinstance(other, self.__class__)) and \
+            (self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        """Checks for inequality"""
+        return not self.__eq__(other)
+
+    def __bool__(self):
+        pass
+
+    def __len__(self):
+        """Return length of Model
+
+        Example:
+            m = Model()
+            len(m)
+
+        Result:
+            int
+        """
+        pass
+
+    def __contains__(self, item):
+        """Checks if model conains an object
+
+        Example:
+            m = Model()
+            t = Object()
+            t in m
+
+        Result:
+            bool
+        """
+        pass
+
+    # Pickle API
+
+    def __getnewargs__(self):
+        pass
+
+    def __getstate__(self):
+        pass
+
+    def setstate__(self, state):
+        pass
+
+
+class Neuron(object):
+    """Neuron representation"""
+
+    def __init__(self):
+        pass

6e1867d6583d856fe4f69ef82a07c44edba1ae84
Sebastian Klatt <sebastian@markow.org>
2014-11-03 13:34:26 +0100

    Refactored preferences panel and gui module

--- pam/__init__.py
+++ pam/__init__.py
@@ -27,9 +27,55 @@ __license__ = bl_info['license']
 __version__ = ".".join([str(s) for s in bl_info['version']])
 
 
+class PAMPreferencesPane(bpy.types.AddonPreferences):
+    """Preferences pane displaying all addon-wide properties.
+
+    Located in
+    `File > User Preferences > Addons > Object: PAM"
+    """
+
+    bl_idname = __package__
+    log_level_items = [
+        ("DEBUG", "(4) DEBUG", "", 4),
+        ("INFO", "(3) INFO", "", 3),
+        ("WARNING", "(2) WARNING", "", 2),
+        ("ERROR", "(1) ERROR", "", 1),
+    ]
+    data_location = bpy.utils.user_resource(
+        "DATAFILES",
+        path=__package__,
+        create=True
+    )
+    log_directory = bpy.props.StringProperty(
+        name="Log Directory",
+        default=data_location,
+        subtype="DIR_PATH",
+        update=utils.log.callback_properties_changed
+    )
+    log_filename = bpy.props.StringProperty(
+        name="Log Filename",
+        default="pam.log",
+        update=utils.log.callback_properties_changed
+    )
+    log_level = bpy.props.EnumProperty(
+        name="Log Level",
+        default="ERROR",
+        items=log_level_items,
+        update=utils.log.callback_properties_changed
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column()
+        col.label(text="Logging:")
+        col.prop(self, "log_directory", text="Directory")
+        col.prop(self, "log_filename", text="Filename")
+        col.prop(self, "log_level", text="Level")
+
+
 def register():
     """Called on enabling this addon"""
-    bpy.utils.register_class(gui.PAMPreferencesPane)
+    bpy.utils.register_class(PAMPreferencesPane)
     utils.log.initialize()
 
     tools.measure.register()
--- pam/gui/panels.py
+++ pam/gui/panels.py
@@ -1,58 +1,6 @@
 """PAM Gui Module"""
 
-import logging
-
 import bpy
-
-from . import utils
-
-logger = logging.getLogger(__package__)
-
-
-class PAMPreferencesPane(bpy.types.AddonPreferences):
-    """Preferences pane displaying all addon-wide properties.
-
-    Located in
-    `File > User Preferences > Addons > Object: PAM"
-    """
-
-    bl_idname = __package__
-    log_level_items = [
-        ("DEBUG", "(4) DEBUG", "", 4),
-        ("INFO", "(3) INFO", "", 3),
-        ("WARNING", "(2) WARNING", "", 2),
-        ("ERROR", "(1) ERROR", "", 1),
-    ]
-    data_location = bpy.utils.user_resource(
-        "DATAFILES",
-        path=__package__,
-        create=True
-    )
-    log_directory = bpy.props.StringProperty(
-        name="Log Directory",
-        default=data_location,
-        subtype="DIR_PATH",
-        update=utils.log.callback_properties_changed
-    )
-    log_filename = bpy.props.StringProperty(
-        name="Log Filename",
-        default="pam.log",
-        update=utils.log.callback_properties_changed
-    )
-    log_level = bpy.props.EnumProperty(
-        name="Log Level",
-        default="ERROR",
-        items=log_level_items,
-        update=utils.log.callback_properties_changed
-    )
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
-        col.label(text="Logging:")
-        col.prop(self, "log_directory", text="Directory")
-        col.prop(self, "log_filename", text="Filename")
-        col.prop(self, "log_level", text="Level")
 
 
 class PAMModelDataPanel(bpy.types.Panel):
@@ -115,9 +63,6 @@ class PAMVisualizeKernelToolsPanel(bpy.types.Panel):
     bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
-
-        # logger.debug("%s", context.blend_data)
-
         active_obj = context.active_object
 
         name = mesh_object_name(active_obj)

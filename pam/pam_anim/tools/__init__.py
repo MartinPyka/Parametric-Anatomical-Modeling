08f629500e96216638bcafd6e9a88b51d6f54600
Martin Pyka <m.pyka@rub.de>
2015-02-04 13:34:13 +0100

    added __init__py in tools-subfolder

---
+++ pam/pam_anim/tools/__init__.py
@@ -0,0 +1,7 @@
+from . import animationTools
+from . import materialTools
+from . import dataTools
+from . import meshTools
+from . import orientationTools
+
+__all__ = ["animationTools", "materialTools", "dataTools", "meshTools", "orientationTools"]

f99bd05d08715fa37d5d5ab7c653ecf0e7823f9c
Sebastian Klatt <sebastian@markow.org>
2015-03-24 16:43:03 +0100

    Restructure unittests

--- build_docs.py
+++ build_docs.py
@@ -9,10 +9,11 @@ except ImportError:
     sys.exit(1)
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-BUILD_DIR = os.path.join(CURRENT_DIR, "_build")
+DOCS_DIR = os.path.join(CURRENT_DIR, "docs")
+BUILD_DIR = os.path.join(DOCS_DIR, "_build")
 
-sys.path.insert(0, os.path.join(CURRENT_DIR, ".."))
+sys.path.insert(0, CURRENT_DIR)
 
 if __name__ == "__main__":
-    argv = ["sphinx-build", "-b", "html", CURRENT_DIR, BUILD_DIR]
+    argv = ["sphinx-build", "-b", "html", DOCS_DIR, BUILD_DIR]
     sphinx.main(argv)
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,10 @@
 
 import unittest
 
-from .test_model import TestModelComparison
+from .test_example import TestExample
 
 
 def main():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestModelComparison))
+    suite.addTest(unittest.makeSuite(TestExample))
     unittest.TextTestRunner(verbosity=2).run(suite)

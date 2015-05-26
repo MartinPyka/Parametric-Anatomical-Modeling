9aabba90920402052b6f33fbe9afdda3e6022123
Patrick Herbers <patrick.herbers@rub.de>
2015-05-26 11:23:41 +0200

    Expanded test to also include random mapping generation.

--- tests/pam_test.py
+++ tests/pam_test.py
@@ -9,9 +9,11 @@ SEED = 42
 
 class TestPamModelCreate(unittest.TestCase):
     def setUp(self):
-        # Import should-be pam model
-        snapshot = pickle.load(open(bpy.path.abspath("//model_test.pam"), "rb"))
+        # Seed the rng
+        random.seed(SEED)
 
+    def loadModel(self, path):
+        snapshot = pickle.load(open(bpy.path.abspath(path), "rb"))
         self.NG_LIST = snapshot.NG_LIST
         self.NG_DICT = snapshot.NG_DICT
         self.CONNECTION_COUNTER = snapshot.CONNECTION_COUNTER
@@ -19,27 +21,31 @@ class TestPamModelCreate(unittest.TestCase):
         self.CONNECTIONS = pam.model.Pickle2Connection(snapshot.CONNECTIONS)
         self.CONNECTION_RESULTS = pam.model.convertArray2Vector(snapshot.CONNECTION_RESULTS)
 
-        # Seed the rng
-        random.seed(SEED)
+    def testModels(self):
+        """Test if the pam model generated using euclidean mapping method is the same as a predefined model.
+
+        Checks CONNECTIONS, CONNECTION_RESULTS, CONNECTION_INDICES, NG_LIST and NG_DICT"""
+        # Import should-be pam model
+        self.loadModel("//model_test.pam")
 
         # Compute mapping
         bpy.ops.pam.mapping_compute()
 
-    def testModel(self):
-        """Test if the pam model generated is the same as a predefined model.
-
-        Checks CONNECTIONS, CONNECTION_RESULTS, CONNECTION_INDICES, NG_LIST and NG_DICT"""
-        
-        self.assertEqual(self.CONNECTIONS, pam.model.CONNECTIONS, "Connections between neuron groups differ")
-        self.assertEqual(len(self.CONNECTION_RESULTS), len(pam.model.CONNECTION_RESULTS), "Connection results do not have the correct length")
-        for i in range(len(self.CONNECTION_RESULTS)):
-            with self.subTest(i=i):
+        for i in range(len(self.CONNECTIONS)):
+            with self.subTest(i = i):
+                self.assertEqual(self.CONNECTIONS[i], pam.model.CONNECTIONS[i], "Connections between neuron groups differ")
                 numpy.testing.assert_array_equal(self.CONNECTION_RESULTS[i]['c'], pam.model.CONNECTION_RESULTS[i]['c'], "Connections are not equal in connection ID " + str(i))
                 numpy.testing.assert_array_equal(self.CONNECTION_RESULTS[i]['d'], pam.model.CONNECTION_RESULTS[i]['d'], "Distances between connections are incorrect in connection ID " + str(i))
                 self.assertEqual(self.CONNECTION_RESULTS[i]['s'], pam.model.CONNECTION_RESULTS[i]['s'], "Synapse vectors are incorrect for connection " + str(i))
-        self.assertEqual(self.CONNECTION_INDICES, pam.model.CONNECTION_INDICES, "Connection indices are incorrect")
-        self.assertEqual(self.NG_LIST, pam.model.NG_LIST, "Neuron group list is incorrect")
-        self.assertEqual(self.NG_DICT, pam.model.NG_DICT, "Neuron group dictionary is incorrect")
+                self.assertEqual(self.CONNECTION_INDICES[i], pam.model.CONNECTION_INDICES[i], "Connection indices are incorrect")
+                self.assertEqual(self.NG_LIST[i], pam.model.NG_LIST[i], "Neuron group list is incorrect")
+            self.assertEqual(self.NG_DICT, pam.model.NG_DICT, "Neuron group dictionary is incorrect")
+
+def run():
+    """Run unittest"""
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestPamModelCreate))
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
-    unittest.main(exit = False, verbosity = 2)
+    run()

1f7bd60414413dfbf40f5ba137ae592bebb2346b
Patrick Herbers <patrick.herbers@rub.de>
2015-06-19 14:05:17 +0200

    Added an option to export a tree to swc
    The tree has to be in its python data format to export

--- pam/trees/uv_tree.py
+++ pam/trees/uv_tree.py
@@ -5,7 +5,7 @@ import bpy
 import mathutils
 from pam import pam
 
-def create_uv_tree(obj, quantity, uv_center, variance = 0.005, balancing_factor = 0.0):
+def create_uv_tree(obj, quantity, uv_center, variance = 0.005, balancing_factor = 0.0, build_type = 'MESH'):
 
     # Generate scattered points
     x = np.random.normal(uv_center[0], variance, quantity)
@@ -27,8 +27,28 @@ def create_uv_tree(obj, quantity, uv_center, variance = 0.005, balancing_factor 
     for i, node in enumerate(nodes):
         node.pos = node_3d_points[i]
 
-    curve_obj = mst_blender.buildTreeCurve(root_point)
-    curve_obj.data.radius = 0.1
+    if build_type == 'CURVE':
+        curve_obj = mst_blender.buildTreeCurve(root_point)
+        curve_obj.data.bevel_depth = 0.1
+    elif build_type == 'MESH':
+        mesh_obj = mst_blender.buildTreeMesh(root_point)
+
+def export_swc(root_node, outfilename, structure_identifier = 2):
+    nodes = mstree.tree_to_list(root_node)
+    f = open(outfilename, 'w')
+    index = 0
+    for node in nodes:
+        node.index = index
+        index += 1
+
+        if node.parent is not None:
+            parent_index = node.parent.index
+        else:
+            parent_index = -1
+
+        f.write(' '.join((str(node.index), str(structure_identifier), str(node.pos[0]), str(node.pos[1]), str(node.pos[2]), str(0), str(parent_index))))
+        f.write('\n')
+    f.close()
 
 if __name__ == '__main__':
     create_uv_tree(bpy.data.objects['CA3_sp_axons_all'], 100, (0.1, 0.2))

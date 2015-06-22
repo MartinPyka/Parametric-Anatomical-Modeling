b2b907e9b058516b403b29e3565d2e96578c0951
Martin Pyka <m.pyka@rub.de>
2015-06-22 11:24:13 +0200

    added function to draw axons for all particles. addressed some bugs but continues interpolation of paths is still missing

--- pam/trees/uv_tree.py
+++ pam/trees/uv_tree.py
@@ -5,33 +5,37 @@ import bpy
 import mathutils
 from pam import pam
 
-def create_uv_tree(obj, quantity, uv_center, mean = [0.0, 0.0], variance = [0.005, 0.005], balancing_factor = 0.0, build_type = 'MESH'):
+def create_uv_tree(obj, quantity, uv_center, mean = [0.0, 0.0], variance = [0.005, 0.005], balancing_factor = 0.0, build_type = 'CURVE'):
 
     # Generate scattered points
-    x = np.random.normal(uv_center[0], variance[0], quantity) + mean[0]
-    y = np.random.normal(uv_center[1], variance[1], quantity) + mean[1]
+    x = [uv_center[0]]
+    y = [uv_center[1]]
+    x = np.concatenate((x, np.random.normal(uv_center[0], variance[0], quantity) + mean[0]))
+    y = np.concatenate((y, np.random.normal(uv_center[1], variance[1], quantity) + mean[1]))
 
     points = np.dstack((x, y))
+    print(points)
     np.clip(points, 0., 1., out = points)
-
+    # all items are stored in the first list-item of the output
+    points = points[0]
+    
     # Run mstree
-    root_point = mstree.mstree(points[0], balancing_factor)
-    print(len(points[0]))
+    root_point = mstree.mstree(points, balancing_factor)
+
     # Convert node positions from uv to 3d
     nodes = mstree.tree_to_list(root_point)
-    print(len(nodes))
-    # print(root_point.pos)
     node_uv_points = [mathutils.Vector((node.pos[0], node.pos[1])) for node in nodes]
+    print(len(node_uv_points))
 
     node_3d_points = pam.mapUVPointTo3d(obj, node_uv_points)
     print(len(node_3d_points))
 
-    for i, node in enumerate(node_3d_points):
+    for i, node in enumerate(nodes):
         node.pos = node_3d_points[i]
 
     if build_type == 'CURVE':
         curve_obj = mst_blender.buildTreeCurve(root_point)
-        curve_obj.data.bevel_depth = 0.1
+        curve_obj.data.bevel_depth = 0.001
     elif build_type == 'MESH':
         mesh_obj = mst_blender.buildTreeMesh(root_point)
 
@@ -57,5 +61,8 @@ def createAxons(p_obj, s_obj, quantity, mean, variance):
         uv = pam.map3dPointToUV(p_obj, s_obj, p.location)
         create_uv_tree(s_obj, quantity, uv, mean, variance)
 
-if __name__ == '__main__':
-    create_uv_tree(bpy.data.objects['CA3_sp_axons_all'], 100, (0.1, 0.2))
+#if __name__ == '__main__':
+#create_uv_tree(bpy.data.objects['CA3_sp_axons_all'], 100, (0.3, 0.5), [-0.1, 0.], [0.02, 0.005])
+ca3 = bpy.data.objects['CA3_sp']
+ca3_s = bpy.data.objects['CA3_sp_axons_all']
+createAxons(ca3, ca3_s, 100, [-0.1, 0.0], [0.02, 0.005])
